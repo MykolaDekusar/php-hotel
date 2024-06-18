4f56c60df04777ffd744073fdf54a15f295cabee
MykolaDekusar <ndekusar97@gmail.com>
2024-06-18 15:10:17 +0200

    [UPD]Added Bootstrap and added hotel info inside table

--- index.php
+++ index.php
@@ -54,38 +54,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel PHP Filter</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Ecco gli Hotel</h1>
-    <?php 
-    foreach($hotels as $data){
-        foreach($data as $info => $key){
-            // if($key == 'parking' && $info != 0){
-            //     echo 'C\'è parcheggio';
-            // }
-            if($info == 'name'){
-                echo "<h2> $key </h2>";
+    <h1 class="text-center my-5">Ecco gli Hotel</h1>
+    <div class="container">
+        <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Nome</th>
+            <th scope="col">Descrizione</th>
+            <th scope="col">Parcheggio</th>
+            <th scope="col" class='text-center'>Voto</th>
+            <th scope="col" class='text-center'>Distanza dal centro</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach($hotels as $data){
+                echo "<tr>";
+                foreach($data as $info => $key){
+                    if($info == 'name'){
+                        echo "<td>$key</td>";
+                    };
+                    if($info == 'description'){
+                        echo "<td>$key</td>";
+                    };
+                    if($info == 'parking'){
+                        if($key != 0){
+                            echo "<td>Disponibile </td>";
+                        }else{
+                            echo "<td><b>Pieno</td>";
+                        }
+                    }
+                    if($info == "vote"){
+                        echo "<td class='text-center'><em>$key</em></td>";
+                    }
+                    if($info == "distance_to_center"){
+                        $value = ceil($key);
+                        echo "<td class='text-center'>$value m</td>";
+                    }
+                };
+                echo "</tr>";
             };
-            if($info == 'description'){
-                echo "<p> $key </p>";
-            };
-            if($info == 'parking'){
-                if($key != 0){
-                    echo "<p>Parcheggio: Disponibile </p>";
-                }else{
-                    echo "<p>Parcheggio: <b>NON</b> Disponibile </p>";
-                }
-            }
-            if($info == "vote"){
-                echo "<p>Voto: <em>$key</em></p>";
-            }
-            if($info == "distance_to_center"){
-                $value = ceil($key);
-                echo "<p>Distanza dal centro: $value </p>";
-            }
-        };
-        echo "<hr>";
-    };
-    ?>
+            ?>
+        </tbody>
+</table>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
